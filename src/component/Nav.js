605e4cd7a08b5e3771e60500a92188c036faa87f
hebrohim <raheemibrohim@mail.com>
2024-01-30 09:43:18 +0100

    fix

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -4,10 +4,10 @@ const Nav = () => {
   return (
     <nav className="px-5 md:px-16 py-5 h-[10vh] flex items-center justify-between">
       <div className="hidden lg:flex w-1/3 justify-between">
-        <a href="#">Home</a>
-        <a href="#">About us</a>
-        <a href="#">Services</a>
-        <a href="#">Portfolio</a>
+        <button>Home</button>
+        <button>About us</button>
+        <button>Services</button>
+        <button>Portfolio</button>
       </div>
       <div className="w-1/3"><img alt="man working on solar panel" src={process.env.PUBLIC_URL + "/images/logo.svg"} /></div>
       <span className="text-[2rem] font-thin text-[#101828] lg:hidden">&#9776;</span>
